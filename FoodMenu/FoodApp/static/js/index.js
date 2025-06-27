314e43be31f12d6e355ce1f9149f1b0c723b83aa
srinidhi <srinidhimiyapuram@gmail.com>
2025-06-27 22:18:38 +0530

    Working on FoodMenu Project

---
+++ FoodMenu/FoodApp/static/js/index.js
@@ -0,0 +1,4 @@
+window.onscroll(() => {
+  const elem = document.getElementsByClassName("item-show");
+  elem[0];
+});
--- FoodMenu/FoodApp/views.py
+++ FoodMenu/FoodApp/views.py
@@ -3,9 +3,13 @@ from .models import FoodItem, WallPaper
 
 # Create your views here.
 def index(request):
-    data = get_wallpaper()
+    data = get_wallpaper("wallpaper2")
+    item_names = ['burger', 'pie']
+    items = get_items(item_names)
+
     return render(request, "index.html", {
         "data": data,
+        'items':items,
     })
 
 
@@ -17,11 +21,22 @@ def get_data():
 
 def get_item(item):
     item = item.capitalize()
-    print(item, " ==================")
     data = FoodItem.objects.filter(name=item)
+    # print(data[1],"======================")
     return data
 
 
-def get_wallpaper():
-    data = WallPaper.objects.all()
+def get_wallpaper(page):
+    data = WallPaper.objects.filter(name=page)
     return data
+
+def get_items(lis):
+    arr = []
+    for item in lis:
+        item = item.capitalize()
+        data = FoodItem.objects.filter(name=item)
+        # print(data[1],"=====================")
+        arr.append(data)
+    # print(arr[0], " ==================")
+    
+    return arr
